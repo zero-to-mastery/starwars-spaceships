d6e43301f457eda35a2ec139e7d6110df553734b
Alin Daniel <adaniel.onea@gmail.com>
2018-04-02 23:05:26 +0300

    addedplanet list

--- src/Components/Planets/Planets.js
+++ src/Components/Planets/Planets.js
@@ -5,17 +5,17 @@ import './Planets.css';
 const Planets = ({name, terrain, population}) => {
   return (
     <div className='tc hashir tc dib br3 pa2 ma4 grow bw2 shadow-5'>
-    	<img alt='planets' className='br3 grow bw2 shadow-5' src='http://via.placeholder.com/200x200' />
-    	<div>
-    		<dl className="lh-title pt3 ph1 mt0">
-    			<dt className='f6 b'>Name</dt>
+        <img alt='planets' className='br3 grow bw2 shadow-5' src='http://via.placeholder.com/200x200' />
+        <div>
+            <dl className="lh-title pt3 ph1 mt0">
+                <dt className='f6 b'>Name</dt>
                 <dd className='ml0'>{name}</dd>
-    			<dt className="f6 b mt2">Terrain</dt>
+                <dt className="f6 b mt2">Terrain</dt>
                 <dd className='ml0'>{terrain}</dd>
-    			<dt className="f6 b mt2">Population</dt>
+                <dt className="f6 b mt2">Population</dt>
                 <dd className='ml0'>{population}</dd>
-    		</dl>
-    	</div>
+            </dl>
+        </div>
     </div>
   );
 }
