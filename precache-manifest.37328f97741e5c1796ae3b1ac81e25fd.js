255b2b3304a4d6a6600de9f7e4fcca9ab1b35ee4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-09 15:21:59 +0000

    Deploy zero-to-mastery/starwars-spaceships to github.com/zero-to-mastery/starwars-spaceships.git:gh-pages

---
+++ precache-manifest.37328f97741e5c1796ae3b1ac81e25fd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "5325230125bef311f6ad",
+    "url": "./static/css/main.f4f2941f.chunk.css"
+  },
+  {
+    "revision": "5325230125bef311f6ad",
+    "url": "./static/js/main.2435b742.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f06c028f49fec3be5236",
+    "url": "./static/css/2.cb4e1db3.chunk.css"
+  },
+  {
+    "revision": "f06c028f49fec3be5236",
+    "url": "./static/js/2.37d0e705.chunk.js"
+  },
+  {
+    "revision": "e4091c2ce0e5be4ecf77802101a305b9",
+    "url": "./static/media/A New Hope.e4091c2c.jpg"
+  },
+  {
+    "revision": "9b9887610c0940a8ca92816b10175038",
+    "url": "./static/media/Alderaan.9b988761.jpg"
+  },
+  {
+    "revision": "619dfcbc421bbda757d7c392ff1c7155",
+    "url": "./static/media/Attack of the Clones.619dfcbc.jpg"
+  },
+  {
+    "revision": "fbd93931f7ca1380abb6017bf065625a",
+    "url": "./static/media/CR90 corvette.fbd93931.jpg"
+  },
+  {
+    "revision": "58795608d50457c340e76d763e73c8d5",
+    "url": "./static/media/Dagobah.58795608.jpg"
+  },
+  {
+    "revision": "586f2ab18e7ac9bf11a807b2516cd2a7",
+    "url": "./static/media/Kamino.586f2ab1.jpg"
+  },
+  {
+    "revision": "09c55f0e97f9464ee5365b770bc32c92",
+    "url": "./static/media/Rebel transport.09c55f0e.jpg"
+  },
+  {
+    "revision": "aa1b3b4441612c69243f2e217d450c49",
+    "url": "./static/media/Return of the Jedi.aa1b3b44.jpg"
+  },
+  {
+    "revision": "cc1a0604c12a4d859c9d5ff5d3bb2c21",
+    "url": "./static/media/Revenge of the Sith.cc1a0604.jpg"
+  },
+  {
+    "revision": "c518d2f8bdccab5e8cf8ca0b686f6302",
+    "url": "./static/media/Star Destroyer.c518d2f8.jpg"
+  },
+  {
+    "revision": "699a5bdbf5511dfeddeab7dd39862237",
+    "url": "./static/media/Tatooine.699a5bdb.jpg"
+  },
+  {
+    "revision": "f0003cacc8a0758a7df018cf0d6374da",
+    "url": "./static/media/The Empire Strikes Back.f0003cac.jpg"
+  },
+  {
+    "revision": "b462115e592cc0669050156e890c8760",
+    "url": "./static/media/The Force Awakens.b462115e.jpg"
+  },
+  {
+    "revision": "1821226055d2127234a435bfb73c5b88",
+    "url": "./static/media/The Phantom Menace.18212260.jpg"
+  },
+  {
+    "revision": "3b8cdd01a8247ded16023eae17035fe1",
+    "url": "./static/media/Bespin.3b8cdd01.png"
+  },
+  {
+    "revision": "567399d64f0ee2d147548cbf804d2b87",
+    "url": "./static/media/Coruscant.567399d6.png"
+  },
+  {
+    "revision": "6c79084ffa275b864f276893d68de5f1",
+    "url": "./static/media/Endor.6c79084f.png"
+  },
+  {
+    "revision": "21d0556357edac975ae628bf4a27cc86",
+    "url": "./static/media/Hoth.21d05563.png"
+  },
+  {
+    "revision": "573010fdcda05da83f741134099f4311",
+    "url": "./static/media/Naboo.573010fd.png"
+  },
+  {
+    "revision": "574e698c5f254383433fdfbb94d1fc15",
+    "url": "./static/media/Yavin IV.574e698c.png"
+  },
+  {
+    "revision": "c77e09ae4e2d1f9d925bebdfe1f239c0",
+    "url": "./static/media/spaceship.c77e09ae.png"
+  },
+  {
+    "revision": "84b965e2d3440a2abba0e50a798d86fa",
+    "url": "./index.html"
+  }
+];
