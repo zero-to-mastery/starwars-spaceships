5bf6c6c976ce00db4ef2821b0f70d1890fad5d99
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-02-11 01:52:13 +0100

    feat: updated to react-tsparticles, react-particles-js is deprecated

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,44 +1,45 @@
 export const particlesoptions = {
-    particles: {
-        "number": {
-            "value": 60,
-            "density": {
-                "enable": true,
-                "value_area": 1500
-            }
-        },
-        "line_linked": {
-            "enable": true,
-            "opacity": 0.02
-        },
-        "move": {
-            "direction": "right",
-            "speed": 0.05
-        },
-        "size": {
-            "value": 1.25
-        },
-        "opacity": {
-            "anim": {
-                "enable": true,
-                "speed": 1.5,
-                "opacity_min": 0.15
-            }
-        }
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        area: 1500,
+      },
     },
-    "interactivity": {
-        "events": {
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            }
-        },
-        "modes": {
-            "push": {
-                "particles_nb": 2
-            }
-        }
+    links: {
+      enable: true,
+      opacity: 0.02,
+      distance: 150,
     },
-    "retina_detect": true
-}
-
+    move: {
+      enable: true,
+      direction: "right",
+      speed: 0.05,
+    },
+    size: {
+      value: 1.25,
+    },
+    opacity: {
+      value: { min: 0.15, max: 0.5 },
+      animation: {
+        enable: true,
+        speed: 1.5,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+    },
+    modes: {
+      push: {
+        quantity: 2,
+      },
+    },
+  },
+  detectRetina: true,
+};
